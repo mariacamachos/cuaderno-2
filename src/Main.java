75ff6bade1738895881e9cb8225863f3fd05c20d
mariacamachos <maria.camachos@edu.uah.es>
2025-02-18 15:59:29 +0100

    MyPoint commit

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,31 @@
-import es.uah.matcomp.mp.e2.ejerciciosclases.e3.Customer;
-import es.uah.matcomp.mp.e2.ejerciciosclases.e3.Account;
+import es.uah.matcomp.mp.e2.ejerciciosclases.e4.MyPoint;
 
 public class Main {
     public static void main(String[] args) {
-// Test Customer class
-        /**Establezco los clientes con su id, nombre y el genero*/
-        Customer c1 = new Customer(99, "Juana", 'f');
-        Customer c2 = new Customer(100, "Laura", 'f');
-        /**Imprimo los datos de los usuarios*/
-        System.out.println(c1);
-        System.out.println(c2);
-        /**No hay set con lo que no puedo probar a establecer un nuevp id o un nuevo nombre para un customer ya definido
-         * si se pudiese lo implementaría de la forma: c1. setId(valor del id nuevo)
-         */
-
-        /**En este caso si hay getters por lo que pruebo que funcionen bien estos lo qu ehacen es devolverme los valores
-         * que ya he establecido en cada usuario
-         */
-        System.out.println("id is:" + c1.getId());
-        System.out.println("name is:" + c1.getName());
-        System.out.println("Gender is:" + c1.getGender());
-
-
-        System.out.println("id is:" + c2.getId());
-        System.out.println("name is:" + c2.getName());
-        System.out.println("Gender is:" + c2.getGender());
-
-        /**Establezco dos cuentas de dos personas*/
-        Account a1 = new Account(99, c1);
-        Account a2 = new Account(100, c2);
-        System.out.println(a1);
-        System.out.println(a2);
-
-        /**Pruebo la parte del codigo del account y establezco dos cuentas*/
-        a1.setBalance(100);
-        System.out.println(a1);
-        a2.setBalance(200);
-        System.out.println(a2);
-
-        /** Imprimo el balance que he establecido arriba*/
-        System.out.println("balance is:" + a1.getBalance());
-        System.out.println("balance is:" + a2.getBalance());
-
-        /** Depósito*/
-        a1.deposit(100);
-        a2.deposit(200);
-        System.out.println(a1);
-        System.out.println(a2);
-
-        /** withdraw*/
-        /**Cuenta 1: tiene 200 y pago 100*/
-        a1.withdraw(100);
-        System.out.println("balance is:" + a1.getBalance());
-        /**Cuenta 2: tiene 400 y paga 100*/
-        a2.withdraw(300);
-        System.out.println("balance is:" + a2.getBalance());
-        /**Cuenta 3: tiene 100 y paga 50*/
-        a1.withdraw(50);
-        System.out.println("balance is:" + a1.getBalance());
-        /** Aquí estoy buscando el error */
-        a2.withdraw(70);
-        System.out.println("balance is:" + a2.getBalance());
-        }
+// Test program to test all constructors and public methods
+        MyPoint p1 = new MyPoint();  // Test constructor
+        System.out.println(p1);
+// Test toString()
+        p1.setX(8);
+        p1.setY(6);
+        // Test setters
+        System.out.println("x is: " + p1.getX());  // Test getters
+        System.out.println("y is: " + p1.getY());
+        p1.setXY(3, 0);
+// Test setXY()
+        System.out.println(p1.getXY()[0]);  // Test getXY()
+        System.out.println(p1.getXY()[1]);
+        System.out.println(p1);
+        MyPoint p2 = new MyPoint(0, 4);  // Test another constructor
+        System.out.println(p2);
+        // Testing the overloaded methods distance()
+        System.out.println(p1.distance(p2));
+// which version?
+        System.out.println(p2.distance(p1));
+// which version?
+        System.out.println(p1.distance(5, 6));  // which version?
+        System.out.println(p1.distance());
+// which version?*/
     }
-
-
+}
 
