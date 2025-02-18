04fab852965bb18988152a4e3d73d513edbd68e8
mariacamachos <maria.camachos@edu.uah.es>
2025-02-18 16:48:56 +0100

    MyLine commit

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,42 @@
 import es.uah.matcomp.mp.e2.ejerciciosclases.e4.MyPoint;
+import es.uah.matcomp.mp.e2.ejerciciosclases.e5.MyLine;
+
 
 public class Main {
     public static void main(String[] args) {
-// Test program to test all constructors and public methods
-        MyPoint p1 = new MyPoint();  // Test constructor
+        MyPoint p1 = new MyPoint(16, 5);
+        MyPoint p2 = new MyPoint(1, 7);
+        MyPoint p3 = new MyPoint(2, 9);
+        MyLine L1 = new MyLine(2,3,1,5);
+        MyLine L2 = new MyLine(p2, p1);
+
         System.out.println(p1);
-// Test toString()
-        p1.setX(8);
-        p1.setY(6);
-        // Test setters
-        System.out.println("x is: " + p1.getX());  // Test getters
-        System.out.println("y is: " + p1.getY());
-        p1.setXY(3, 0);
-// Test setXY()
-        System.out.println(p1.getXY()[0]);  // Test getXY()
-        System.out.println(p1.getXY()[1]);
-        System.out.println(p1);
-        MyPoint p2 = new MyPoint(0, 4);  // Test another constructor
-        System.out.println(p2);
-        // Testing the overloaded methods distance()
-        System.out.println(p1.distance(p2));
-// which version?
-        System.out.println(p2.distance(p1));
-// which version?
-        System.out.println(p1.distance(5, 6));  // which version?
-        System.out.println(p1.distance());
-// which version?*/
+        System.out.println(L1);
+
+        System.out.println(L2.getBegin());
+        System.out.println(L2.getEnd());
+        System.out.println(L1.getBeginX());
+        System.out.println(L1.getBeginY());
+        System.out.println(L1.getEndX());
+        System.out.println(L1.getEndY());
+
+        L1.setBeginX(p1.getX());
+        System.out.println(L1);
+        L1.setBeginY(p1.getY());
+        System.out.println(L1);
+        L1.setBegin(p2);
+        System.out.println(L1);
+        L1.setEnd(p3);
+        System.out.println(L1);
+
+        L1.setEndXY(p3.getX(), p2.getY());
+        System.out.println(L1);
+        L1.getBeginXY();
+        L2.setEndXY(p1.getX(), p2.getY());
+        System.out.println(L2);
+        L2.getBeginXY();
+        System.out.println(L2.getLength());
+        System.out.println(L1.getLength());
+
     }
 }
-
