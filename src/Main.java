874409a24bb8dee31dc03a2a5926917952ea5788
mariacamachos <maria.camachos@edu.uah.es>
2025-02-20 19:25:07 +0100

    Comentado MyCircle y el main

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,60 @@
+/**Se importan las clases MyPoint.java y MiCircle.java*/
 import es.uah.matcomp.mp.e2.ejerciciosclases.e4.MyPoint;
-import es.uah.matcomp.mp.e2.ejerciciosclases.e5.MyLine;
+import es.uah.matcomp.mp.e2.ejerciciosclases.e6.MyCircle;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /**Construimos un programa para calcular con los atributos y metodos*/
-        /**Establezco los tres puntos de coordenadas (x,y) a partir de importar las clase de
-         * MyPoint.java y MyLine.java */
-        MyPoint p1 = new MyPoint(16, 5);
-        MyPoint p2 = new MyPoint(1, 7);
-        MyPoint p3 = new MyPoint(2, 9);
-        /**Designamos la línea 1 designada por el punto inicial (2,3) y el punto
-         * final designado por el (1,5)*/
-        MyLine L1 = new MyLine(2,3,1,5);
-        /**La línea 2 está formada por el punto p1 y p2 designados arriba con el MyPoint*/
-        MyLine L2 = new MyLine(p2, p1);
+        /**Establecemos un punto cuyas coordenadas son (1,9)*/
+        MyPoint p1 = new MyPoint(1, 9);
+        /**Creamos tres círculos, el primero con centro en (1,3) y radio 5,
+         * el segundo con centro (9,3) y radio 3 y, el último, centro en (0,0) y radio 1*/
+        MyCircle c1 = new MyCircle(1, 3, 5);
+        MyCircle c2 = new MyCircle(new MyPoint(9, 3), 3);
+        MyCircle c3 = new MyCircle();
+        /**Se imprimen los datos del círculo 1, 2 y 3*/
+        System.out.println("Circulo 1: " + c1);
+        System.out.println("Circulo 2: " + c2);
+        System.out.println("Circulo 3: " + c3);
 
-        /**Se imprimen los puntos y las líneas */
-        System.out.println("El punto 1 es:" + p1);
-        System.out.println("El punto 2 es:" + p2);
-        System.out.println("La línea 1 es:" + L1);
-        System.out.println("La línea 2 es:" + L2);
+        /**Se imprimen los datos del centro del circulo 1 y el radio del circulo 2*/
+        System.out.println("Centro de c1: " + c1.getCenter());
+        System.out.println("Radio de c2: " + c2.getRadius());
 
-        /**Se muestra la información de las líneas*/
-        System.out.println("El punto inicial de la línea 2 es:" + L2.getBegin());
-        System.out.println("El punto final de la línea 2 es:" + L2.getEnd());
-        System.out.println("La coordenada x del punto inicial de la línea 1 es:" + L1.getBeginX());
-        System.out.println("La coordenada y del punto inicial de la línea 1 es:" +L1.getBeginY());
-        System.out.println("La coordenada x del punto final de la línea 1 es:" + L1.getEndX());
-        System.out.println("La coordenada y del punto final de la línea 1 es:" + L1.getEndY());
+        /**Establecemos para el radio del circulo 1 el valor de: 5 y lo imprimimos*/
+        c1.setRadius(5);
+        System.out.println("Nuevo radio de c1: " + c1.getRadius());
+        /**Establecemos para el centro del circulo 1 el punto: (6,4) y lo imprimimos*/
+        c1.setCenter(new MyPoint(6, 4));
+        System.out.println("Nuevo centro de c1: " + c1.getCenter());
 
-        /** A través de los setter se modifican datos en concreto y se imprime a nueva información sobre la L1*/
-        L1.setBeginX(p1.getX());
-        System.out.println("La nueva línea 1 es (ha cambiado la coordenada x del punto inicial):" + L1);
-        L1.setBeginY(p1.getY());
-        System.out.println("La nueva línea 1 es (ha cambiado la coordenada y del punto inicial):" + L1);
-        L1.setBegin(p2);
-        System.out.println("La nueva línea 1 es (se le ha asignado el p2 al punto de inicio): " + L1);
-        L1.setEnd(p3);
-        System.out.println("La nueva línea 1 es (se le ha asignado el p3 al punto del final):" + L1);
+        /**Establecemos el centro del circulo 1 con la modificacion de la
+         * coordenada x en 2 y lo imprimimos*/
+        c1.setCenterX(2);
+        System.out.println("Nuevo centro de c1: " + c1.getCenter());
+        /**Establecemos el centro del circulo 2 con la modificacion de la
+         * coordenada y en 5 y lo imprimimos*/
+        c2.setCenterY(5);
+        System.out.println("Nuevo centro de c2: " + c2.getCenter());
 
-        /**Modifica las coordenadas de las líneas*/
-        L1.setEndXY(p3.getX(), p2.getY());
-        /**En la coordenada final se el asigna la x del p3 y la y del p2 y se imprime*/
-        System.out.println("La nueva línea 1 es:" + L1);
+        /**Cambiamos las coordenadas del centro del circulo 1 en: (4,9) y lo imprimimos*/
+        c1.setCenterXY(4, 9);
+        System.out.println("Nuevo centro de c1: " + Arrays.toString(c1.getCenterXY()));
+        /**Cambiamos las coordenadas del centro del circulo 2 en: (0,6) y lo imprimimos*/
+        c2.setCenterXY(0, 6);
+        System.out.println("Nuevo centro de c2: " + Arrays.toString(c2.getCenterXY()));
 
-        /**En la coordenada final se el asigna la x del p1 y la y del p2 y se imprime*/
-        L2.setEndXY(p1.getX(), p2.getY());
-        System.out.println(L2);
+        /**Imprime el area del circulo 1 y 2*/
+        System.out.println("Area de c1: " + c1.getArea());
+        System.out.println("Area de c2: " + c2.getArea());
 
+        /**Imprime la circunferencia del circulo 1 y 2*/
+        System.out.println("Circunferencia de c1: " + c1.getCircumference());
+        System.out.println("Circunferencia de c2: " + c2.getCircumference());
 
-        /**Imprime la longitud de ambas líneas*/
-        System.out.println("La longitud de la línea 2 es:" + L2.getLength());
-        System.out.println("La longitud de la línea 1 es:" + L1.getLength());
-
+        /**Imprime la distancia entre los dos circulos */
+        System.out.println("Distancia entre los centros del circulo 1 y 2: " + c1.distance(c2));
     }
 }
-
-
 
 
--- src/es/uah/matcomp/mp/e2/ejerciciosclases/e6/MyCircle.java
+++ src/es/uah/matcomp/mp/e2/ejerciciosclases/e6/MyCircle.java
@@ -1,64 +1,82 @@
 package es.uah.matcomp.mp.e2.ejerciciosclases.e6;
 
 import es.uah.matcomp.mp.e2.ejerciciosclases.e4.MyPoint;
-
-public class MyCircle {
+/**Establece el círculo a partir del centro y el radio*/
+public class MyCircle {/**Guardado en MyCircle.java*/
+    /**ATRIBUTOS*/
+    /** Establecidos de manera privada */
     private MyPoint center;
     private double radius;
-
+    /**METODOS*/
+    /** Construimos el metodo de mi circulo a partir del punto designado por MyPoint como el centro y de radio 1*/
     public MyCircle(){
         this.center = new MyPoint();
         this.radius = 1;
     }
+    /** Construimos el metodo de mi circulo a partir del punto designado por MyPoint como el centro y un radio a designar */
     public MyCircle(int x, int y, int radius){
         this.center = new MyPoint(x, y);
         this.radius = radius;
     }
+    /** Construimos el metodo de mi circulo a partir de un centro y un radio a establecer */
     public MyCircle(MyPoint center, int radius){
         this.center = center;
         this.radius = radius;
     }
+    /**Devuelve el radio*/
     public int getRadius(){
         return (int) radius;
     }
+    /**Establece un radio determinado*/
     public void setRadius(int radius){
         this.radius = radius;
     }
+    /**Devuelve el centro*/
     public MyPoint getCenter(){
         return center;
     }
+    /**Establece un centro determinado*/
     public void setCenter(MyPoint center){
         this.center = center;
     }
+    /**Devuelve la coordenada x del centro del cículo*/
     public int getCenterX(){
         return center.getX();
     }
+    /**Establece una nueva coordenada x del centro del cículo*/
     public void setCenterX(int x){
         this.center.setX(x);
     }
+    /**Devuelve la coordenada y del centro del cículo*/
     public int getCenterY(){
         return center.getY();
     }
+    /**Establece una nueva coordenada y del centro del cículo*/
     public void setCenterY(int y){
         this.center.setY(y);
     }
+    /**Devuelve las coordenadas [x,y] del centro del circulo*/
     public int[] getCenterXY(){
         return new int[]{getCenterX(), getCenterY()};
     }
+    /**Establece nuevas las coordenadas del centro del circulo */
     public void setCenterXY(int x, int y){
         this.center.setX(x);
         this.center.setY(y);
     }
-    @Override
+    /**Devuelce la cadena de la forma: "MyCircle{radius=R, center=(X,Y)} */
     public String toString(){
         return "MyCircle{radius=" + radius + ", center=" + center + "}';";
     }
+    /**Devuelve el area del circulo*/
     public double getArea(){
         return Math.PI * radius * radius;
     }
+    /**Devuelve la circunferencia del circulo*/
     public double getCircumference(){
         return 2 * Math.PI * radius;
     }
+    /**Devuelve la distancia entre los centros de dos circulos*/
     public double distance(MyCircle another){
         return getCenterX() - another.getCenterX();
     }
